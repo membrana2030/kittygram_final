a484d5f92a2bf068f3271228b604dcb54e0ccb12
membrana2030 <lionizo2030@gmail.com>
2025-01-11 17:12:31 +0200

    9

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from cats.views import AchievementViewSet, CatViewSet
 
+from rest_framework import routers
+
+from cats.views import AchievementViewSet, CatViewSet
 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
@@ -18,5 +19,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
