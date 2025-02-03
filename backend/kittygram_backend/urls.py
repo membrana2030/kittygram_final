3c00523c49720ef8ccfc6f95acfe70739cea5290
membrana2030 <lionizo2030@gmail.com>
2025-02-03 20:21:33 +0200

    16

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('api/', include('djoser.urls.authtoken')),
 ]
 
-urlpatterns += static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
