6062f82d8fb934e8668e404a9b212586ebb22609
membrana2030 <lionizo2030@gmail.com>
2025-02-04 14:51:20 +0200

    23

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "false").strip().lower() == "true"
 
-ALLOWED_HOSTS = ['89.169.165.123', 'localhost', '127.0.0.1', 'kittygrammembrana.hopto.org', 'backend', '0.0.0.0']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
